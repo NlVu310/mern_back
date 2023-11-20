b24ddce6b7ff5f2b418792742ef4d05e381bc539
NlVu310 <vubeo310@gmail.com>
2023-11-20 15:47:08 +0700

    commit vid 59 p30

--- src/models/OrderProductModel.js
+++ src/models/OrderProductModel.js
@@ -39,5 +39,5 @@ const orderSchema = new mongoose.Schema({
     }
 );
 
-const Order = mongoose.model('Model', orderSchema);
+const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,34 +1,106 @@
 const Order = require('../models/OrderProductModel')
-// const bcrypt = require("bcrypt")
-// const { genneralAccessToken, genneralRefreshToken } = require('./JwtService')
+const Product = require('../models/ProductModel')
+
 
 const createOrder = (newOrder) => {
     return new Promise(async (resolve, reject) => {
         const { orderItems, paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone, user } = newOrder
-        const createdOrder = await Order.create({
-            orderItems,
-            shippingAddress: {
-                fullName,
-                address,
-                city, phone
-            },
-            paymentMethod,
-            itemsPrice,
-            shippingPrice,
-            totalPrice,
-            user: user,
-        })
-        console.log(createdOrder)
-        if (createdOrder) {
-            resolve({
-                status: 'OK',
-                message: 'Success',
-                data: createdOrder,
+        try {
+            const promises = orderItems.map(async (order) => {
+                const productData = await Product.findOneAndUpdate(
+                    {
+                        _id: order.product,
+                        countInStock: { $gte: order.amount }
+                    },
+                    {
+                        $inc: {
+                            countInStock: -order.amount,
+                            selled: +order.amount
+                        }
+                    },
+                    { new: true }
+                )
+                if (productData) {
+                    return {
+                        status: 'OK',
+                        message: 'SUCCESS'
+                    }
+                }
+                else {
+                    return {
+                        status: 'OK',
+                        message: 'ERR',
+                        id: order.product
+                    }
+                }
             })
+            const results = await Promise.all(promises)
+            const newData = results && results.filter((item) => item.id)
+            if (newData.length) {
+                const arrId = []
+                newData.forEach((item) => {
+                    arrId.push(item.id)
+                })
+                resolve({
+                    status: 'ERR',
+                    message: `San pham voi id: ${arrId.join(',')} khong du hang`
+                })
+            } else {
+                const createdOrder = await Order.create({
+                    orderItems,
+                    shippingAddress: {
+                        fullName,
+                        address,
+                        city, phone
+                    },
+                    paymentMethod,
+                    itemsPrice,
+                    shippingPrice,
+                    totalPrice,
+                    user: user,
+                    // isPaid, paidAt
+                })
+                if (createdOrder) {
+                    // await EmailService.sendEmailCreateOrder(email,orderItems)
+                    resolve({
+                        status: 'OK',
+                        message: 'success'
+                    })
+                }
+            }
+        } catch (e) {
+            //   console.log('e', e)
+            reject(e)
         }
     })
 }
 
+const getOrderDetails = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const order = await Order.findOne({
+                user: id
+            })
+            if (order === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The order is not defined'
+                })
+            }
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: order
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+
 module.exports = {
-    createOrder
+    createOrder,
+    getOrderDetails
 }
