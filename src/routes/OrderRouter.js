52112dcda8946752d4e27c107d5d8cf85caac424
NlVu310 <vubeo310@gmail.com>
2023-11-28 11:28:43 +0700

    commit done

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const OrderController = require('../controllers/OrderController')
-const { authUserMiddleWare } = require("../middleware/authMiddleware")
+const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddleware")
 
 router.post('/create/:id', authUserMiddleWare, OrderController.createOrder)
-router.get('/get-all-order/:id', authUserMiddleWare, OrderController.getAllOrderDetails)
+router.get('/get-all-order-by-id/:id', authUserMiddleWare, OrderController.getAllOrderDetails)
 router.get('/get-details-order/:id', OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id', authUserMiddleWare, OrderController.cancelOrderDetails)
+router.get('/get-all-order', OrderController.getAllOrder)
 
 
 module.exports = router     
--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -177,9 +177,26 @@ const cancelOrderDetails = (id, data) => {
     })
 }
 
+
+const getAllOrder = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allOrder = await Order.find().sort({ createdAt: -1, updatedAt: -1 })
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allOrder
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     createOrder,
     getAllOrderDetails,
     getDetailsOrder,
-    cancelOrderDetails
+    cancelOrderDetails,
+    getAllOrder
 }
